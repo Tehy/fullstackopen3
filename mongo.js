bcda84466626cc823276dddc24df64f4988a7f14
k1921 <k1921@student.jamk.fi>
2020-05-31 15:45:01 +0300

    fix lint

--- mongo.js
+++ mongo.js
@@ -23,7 +23,7 @@ if (process.argv.length === 5) {
     name: process.argv[3],
     number: process.argv[4],
   });
-  person.save().then((response) => {
+  person.save().then(() => {
     console.log(
       "added ",
       person.name,
