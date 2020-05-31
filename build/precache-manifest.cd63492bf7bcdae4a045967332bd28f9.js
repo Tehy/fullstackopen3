52e216dc262339ac59e1b32cafae09f22d9a7058
k1921 <k1921@student.jamk.fi>
2020-05-31 15:14:00 +0300

    add build

---
+++ build/precache-manifest.cd63492bf7bcdae4a045967332bd28f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25ffae6b4dc61eaca5ec4a6b2f4b303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98900577be08857038d7",
+    "url": "/static/css/main.f55c915e.chunk.css"
+  },
+  {
+    "revision": "452e8dca2de5373cdf63",
+    "url": "/static/js/2.981d9bc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.981d9bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98900577be08857038d7",
+    "url": "/static/js/main.03173e2a.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
